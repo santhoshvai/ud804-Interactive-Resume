971f0e0b5c8556bfbe688a6eeb86bedd6b9637a2
santhosh <santhoshvai@gmail.com>
2015-04-30 23:51:47 +0200

    added the needed json format

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,5 @@
 // jQuery to raw js http://www.sitepoint.com/jquery-vs-raw-javascript-1-dom-forms/
+// json needed format https://raw.githubusercontent.com/cherylcourt/resume/gh-pages/js/resumeBuilder.js
 var formattedName = HTMLheaderName.replace("%data%", "Santhosh");
 var formattedRole = HTMLheaderRole.replace("%data%", "Full Stack Developer");
 
